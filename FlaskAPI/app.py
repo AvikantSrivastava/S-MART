d772aa20db910e068a65bc3ec408fb4e53d75004
Avikant Srivastava <asdavikant@gmail.com>
2020-12-15 03:30:46 +0530

    Post request are now working 🍻🍻

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+from pyzbar.pyzbar import decode
+# from PIL import image
 
 app = Flask('Mart API')
 
-if __name__ =='__main__':
-  app.run(debug=True)
+
+@app.route('/api/barcode', methods=['POST'])
+def barcode():
+    if request.method == 'POST':
+
+      # testing dummy post request 
+
+      # Write this in your terminal
+      
+      # # curl -X POST -F 'dummy=avikant' http://127.0.0.1:5000/api/barcode
+      
+      # uncomment the following lines to check post request
+      dummyText = request.form.get('dummy')
+      print(dummyText)
+      return 'Okay daddy\n'
+
+      # barcodeImageEncoded = request.form.get('barcode_encoded_image')
+      
+
+if __name__ == '__main__':
+    app.run(debug=True)
